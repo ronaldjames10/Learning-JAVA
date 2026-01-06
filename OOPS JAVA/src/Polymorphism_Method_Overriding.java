2b810f3d206da5bfdd10527b9fa554d41b9e6a5c
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-01-07 00:11:30 +0530

    Add files via upload

---
+++ OOPS JAVA/src/Polymorphism_Method_Overriding.java
@@ -0,0 +1,14 @@
+// Parent class
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
