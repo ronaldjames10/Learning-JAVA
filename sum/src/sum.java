be0e8640c151b79752b27f1c05691a1191eb177e
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-01-08 23:39:33 +0530

    Add files via upload

---
+++ sum/src/sum.java
@@ -0,0 +1,16 @@
+public class sum {
+
+    public static void main(String[] args) {
+
+
+        int num1 = 665 ;
+        int num2 = 4 ;
+        int num3 = 5 ;
+        int sum = num1 + num2 + num3;
+
+        System.out.println(sum);
+
+
+    }
+
+}
