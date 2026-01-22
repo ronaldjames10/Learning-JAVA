9703cdaffc00bf9bbb0b188dc6d39fdaf87aacbf
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-01-22 23:42:56 +0530

    Add files via upload

---
+++ DSA/src/LC_904.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int left = 0;
+        int maxFruits = 0;
+        Map<Integer, Integer> basket = new HashMap<>();
+
+        for (int right = 0; right < fruits.length; right++) {
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // If more than 2 fruit types, shrink window
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+                if (basket.get(fruits[left]) == 0) {
+                    basket.remove(fruits[left]);
+                }
+                left++;
+            }
+
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        return maxFruits;
+    }
+}
