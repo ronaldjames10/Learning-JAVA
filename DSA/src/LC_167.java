f66aaf43ef70f130864ee19e06ea1c91bf7ee137
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-01-22 23:41:38 +0530

    Add files via upload

---
+++ DSA/src/LC_167.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] num, int target) {
+        int i=0;
+        int j= num.length - 1;
+        while(i < j){
+            int sum = num [i] + num [j];
+            if (sum  == target){
+                return new int[]{i + 1, j + 1};
+            }
+            else if (sum < target){
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
