0f27ef9b287cbf5646c87b2f578524a694632501
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-01-23 23:46:41 +0530

    Add files via upload

---
+++ DSA/src/LC_424.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+        int result = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'A';
+            freq[index]++;
+
+            maxFreq = Math.max(maxFreq, freq[index]);
+
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
+        
+ 
